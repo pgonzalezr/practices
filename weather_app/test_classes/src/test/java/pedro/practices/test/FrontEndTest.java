080c891b523ac2cd8b2f38b3919af2ae503e19e6
Pedro <pgonzalez@tacitknowledge.com>
2014-07-16 14:49:55 -0500

    Adding more steps for my test

--- weather_app/test_classes/src/test/java/pedro/practices/test/FrontEndTest.java
+++ weather_app/test_classes/src/test/java/pedro/practices/test/FrontEndTest.java
@@ -4,6 +4,7 @@ import org.junit.After;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import pedro.practices.DriverFactory;
+import pedro.practices.ElementFinder;
 
 import java.net.MalformedURLException;
 
@@ -11,10 +12,16 @@ import java.net.MalformedURLException;
  * Created by pgonzalez on 7/16/14.
  */
 public class FrontEndTest {
-    private static WebDriver webDriver;
+    // Element locators:
+    private static final String searchBox = "#nav-search.pull-right.show.hidden";
+    private static final String searchTrigger = "span.input-group-btn button.btn.btn-default";
+
+    private static WebDriver webDriver = null;
+    private static ElementFinder elementFinder = null;
 
     public FrontEndTest() throws MalformedURLException {
         webDriver = DriverFactory.getDriver();
+        elementFinder = new ElementFinder();
     }
 
     @After
@@ -23,13 +30,24 @@ public class FrontEndTest {
     }
 
     @Test
-    public void validateCitySearch() {
+    public void validateCitySearch() throws InterruptedException {
         goToWeatherSearch();
+        searchForCity();
     }
 
     private void goToWeatherSearch() {
         final String weatherSearchUrl = System.getProperty("page.url");
         webDriver.navigate().to(weatherSearchUrl);
+    }
+
+    private void searchForCity() throws InterruptedException {
+        final String city = System.getProperty("city");
+        elementFinder.getElementByCSS(searchBox, webDriver).click();
+        Thread.sleep(1000);
+        elementFinder.getElementByCSS(searchBox, webDriver).click();
+        elementFinder.getElementByCSS(searchBox, webDriver).clear();
+        elementFinder.getElementByCSS(searchBox, webDriver).sendKeys(city);
+        elementFinder.getElementByCSS(searchTrigger, webDriver).click();
     }
 
 }
